5749eedc9b3a1174a3bba81072c3f8ecfe3b174f
Jay <31777481+iaevelis@users.noreply.github.com>
2017-10-07 19:06:57 -0700

    Create 2.speed.js

---
+++ 2.speed.js
@@ -0,0 +1,61 @@
+var agents = [];
+
+function setup() {
+  createCanvas(400, 400);
+  for (var i = 0; i < 30; i++) {
+    agents.push({
+      x: random(width),
+      y: random(height),
+      angle: random(TWO_PI),
+      state: floor(random(3)),
+			h: random(10,100)
+    });
+  }
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(220);
+  agents.forEach(activateAgent);
+  agents.forEach(drawAgent);
+}
+
+function drawAgent(agent) {
+  push();
+  translate(agent.x, agent.y);
+  rotate(agent.angle);
+  stroke(0);
+  fill(agent.h,agent.h,150,);
+  rect(0, 0, 20, 10);
+  fill(0);
+  rect(0, 0, 2, 2);
+  pop();
+	colorMode(HSB)
+}
+
+function activateAgent(agent) {
+  if (random(100) < 1) {
+    agent.state = floor(random(3));
+  }
+
+  if (agent.state == 1) {
+    agent.angle += PI / 100;
+  }
+
+  if (agent.state == 2) {
+    agent.x += cos(agent.angle) / 1;
+    agent.y += sin(agent.angle) / 1;
+
+    if (agent.x > width) {
+      agent.x = 0;
+    } else if (agent.x < 0) {
+      agent.x = width;
+    }
+
+    if (agent.y > height) {
+      agent.y = 0;
+    } else if (agent.y < 0) {
+      agent.y = height;
+    }
+  }
+}
