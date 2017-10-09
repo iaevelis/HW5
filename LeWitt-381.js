b3afd6c80e3cda3684bfc5715d672cc32590d123
Jay <31777481+iaevelis@users.noreply.github.com>
2017-10-08 17:03:23 -0700

    Create LeWitt-381.js

---
+++ LeWitt-381.js
@@ -0,0 +1,27 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+	colorMode(RGB)
+  background(255);
+
+
+	noStroke();{
+	rect(50,50,150,150);
+	fill(220,84,120);
+}
+	noStroke();{
+	rect(50,200,150,150);
+	fill(250,233,122);
+}
+	noStroke();{
+	rect(200,50,150,150);
+	fill(73,170,212);
+}
+	noStroke();{
+	rect(200,200,150,150);
+	fill(200);
+}
+	
+}
