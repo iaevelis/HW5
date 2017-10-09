571fb0c657c3abe33a018df6b61d9a3e20049b75
Jay <31777481+iaevelis@users.noreply.github.com>
2017-10-08 17:00:39 -0700

    Create LeWitt-365.js

---
+++ LeWitt-365.js
@@ -0,0 +1,27 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+	colorMode(RGB)
+  background(255);
+
+
+	noStroke();{
+	rect(50,50,150,150);
+	fill(150);
+}
+	noStroke();{
+	rect(50,200,150,150);
+	fill(150);
+}
+	noStroke();{
+	rect(200,50,150,150);
+	fill(100);
+}
+	noStroke();{
+	rect(200,200,150,150);
+	fill(200);
+}
+	
+}
